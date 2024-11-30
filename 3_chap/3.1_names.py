af386b06ebce6774e6eb3d0d971ab55b50066468
Ilya <iam.goryn@gmail.com>
2024-11-30 19:15:57 +0300

    .\3.1_names.py

---
+++ 3_chap/3.1_names.py
@@ -0,0 +1,15 @@
+names = ['leonid', 'ivan', 'roma', 'max', 'kirill']
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+print(names[4].title())
+
+print()
+
+print(f"{names[0].title()}, hello! How are you?")
+print(f"{names[1].title()}, hello! How are you?")
+print(f"{names[2].title()}, hello! How are you?")
+print(f"{names[3].title()}, hello! How are you?")
+print(f"{names[4].title()}, hello! How are you?")
