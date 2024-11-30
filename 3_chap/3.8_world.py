8956064107dcee14f94ec5adaf231310a33d3fce
Ilya <iam.goryn@gmail.com>
2024-11-30 19:16:51 +0300

    .\3.8_world.py

---
+++ 3_chap/3.8_world.py
@@ -0,0 +1,18 @@
+country = ['USA', 'UK', 'Spain', 'Sweden', 'Norway', 'Germany']
+
+print(country)
+
+sorted_country = sorted(country)
+print(sorted_country)
+
+print(country)
+
+country.reverse()
+print(country)
+country.reverse()
+print(country)
+
+country.sort()
+print(country)
+country.sort(reverse=True)
+print(country)
