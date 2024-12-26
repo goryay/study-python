efd0e7e29cd9b8c28d6ebce6e4317f7f2942ea5c
Ilya <iam.goryn@gmail.com>
2024-12-26 23:57:26 +0300

    create a function to return questions and copy the old list to the new one.

--- chap_8/message_sending_8_10.py
+++ chap_8/message_sending_8_10.py
@@ -1,8 +1,24 @@
-def print_message(messages, sent):
-messages = ['Hello!', 'Hi', 'How are u?', 'How old are u?']
-
-def show_messages(messages):
+def print_message(messages, messages_send):
     for message in messages:
         print(message)
+        messages_send.append(message)
 
-show_messages(messages)
+
+def send_messages(messages, messages_send):
+    for message in messages:
+        print(f"Sending message: {message}")
+        messages_send.append(message)
+
+
+messages = ['Hello!', 'Hi', 'How are u?', 'How old are u?']
+messages_send = []
+
+messages_copy = messages[:]
+
+print_message(messages, messages_send)
+send_messages(messages_copy, messages_send)
+
+print(f"\n{messages}")
+print(messages_send)
+print(f"\nOriginal messages: {messages}")
+print(f"Sent messages: {messages_send}")
