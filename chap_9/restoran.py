56bd1c7208bbc0ad221540bf476d61578403d9aa
Ilya <iam.goryn@gmail.com>
2024-12-27 00:21:36 +0300

    import from 9_6

---
+++ chap_9/restoran.py
@@ -0,0 +1,7 @@
+from ice_cream_stand_9_6 import Restaurant, IceCreamStand
+
+restaurant_info = Restaurant("Chicken", "Mexican")
+ice_cream_list = IceCreamStand("Sweet Treats", "Ice Cream")
+
+print(restaurant_info.describe_restaurant())
+print(ice_cream_list.describe_restaurant())
