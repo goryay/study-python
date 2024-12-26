96bf2ded4d5b45b7ea3367bb3824d6281040d7fe
Ilya <iam.goryn@gmail.com>
2024-12-26 23:53:33 +0300

    create a function with standard conditions and make it possible to specify your own conditions

---
+++ chap_8/cities_8_5.py
@@ -0,0 +1,6 @@
+def describe_city(city_name='Moscow', country='Russia'):
+    print(f"The {city_name} city is {country}")
+
+describe_city()
+describe_city(city_name='New-York', country='USA')
+describe_city(city_name='London', country='UK')
