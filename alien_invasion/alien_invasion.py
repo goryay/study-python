729c94c6109748fa8940ffd0d43d205b5d875bd9
Ilya <iam.goryn@gmail.com>
2025-01-20 23:50:48 +0300

    fullscreen. q to quit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,10 +12,13 @@ class AlienInvasion:
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('Alien Invasion')
 
-        self.ship = Ship(
-            self)  # В книге дан пример того, что тут screen нужно писать. Но по факту, тут self. Нужна не переменная, а экземпляр.
+        # В книге дан пример того, что тут screen нужно писать. Но по факту, тут self. Нужна не переменная, а экземпляр.
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
