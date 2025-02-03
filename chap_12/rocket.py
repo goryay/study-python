d8c19061a1f0182ee680ed96ddec61a78d2cc927
Ilya <iam.goryn@gmail.com>
2025-02-03 23:01:35 +0300

    added rocket

---
+++ chap_12/rocket.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Rocket:
+    def __init__(self, bs_window):
+        self.screen = bs_window.screen
+        self.screen_rect = self.screen.get_rect()
+        self.image = pygame.image.load('images/rocket.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = self.screen_rect.center
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 1
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 1
+        if self.moving_up and self.rect.top > 0:
+            self.rect.y -= 1
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += 1
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
