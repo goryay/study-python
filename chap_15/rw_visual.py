65ca8496312380768bf0c98f7f3901ff52cd15ae
Ilya <iam.goryn@gmail.com>
2025-02-24 22:22:03 +0300

    formatting the start and end points

--- chap_15/rw_visual.py
+++ chap_15/rw_visual.py
@@ -3,9 +3,17 @@ from random_walk import RandomWalk
 
 rw = RandomWalk()
 rw.fill_walk()
+while True:
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.set_aspect('equal')
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    plt.show()
+    keep_running = input("Do you want to continue? [y/n]: ")
+    if keep_running == 'n':
+        break
