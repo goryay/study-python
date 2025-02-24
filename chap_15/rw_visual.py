4cc65595af545bb34d92d5e118d48fb1f4cdf86e
Ilya <iam.goryn@gmail.com>
2025-02-24 22:26:04 +0300

    refactoring

--- chap_15/rw_visual.py
+++ chap_15/rw_visual.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
 while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
