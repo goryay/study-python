7fbf054cf2d776e29abe0288ce7f0ca103d19fc5
Ilya <iam.goryn@gmail.com>
2025-02-24 23:16:27 +0300

    two-dice roll

--- chap_15/die_visual.py
+++ chap_15/die_visual.py
@@ -1,17 +1,19 @@
 import plotly.express as px
 from die import Die
 
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # print(results)
 
 frequencies = []
-poss_results = range(1, die.num_sides + 1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -19,7 +21,7 @@ for value in poss_results:
 print(frequencies)
 
 fig = px.bar(x=poss_results, y=frequencies)
-title = "Results of Rolling One D6 1000 Times"
+title = "Results of Rolling Two D6 1000 Times"
 lables = {'x': 'Result', 'y': 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=lables)
 fig.show()
