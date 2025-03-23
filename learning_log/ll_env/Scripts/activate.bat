fdd06aea92dc92314ef6bd4955ff7aeb5a6f3e27
Ilya <iam.goryn@gmail.com>
2025-03-23 13:24:16 +0300

    project refactoring

--- learning_log/ll_env/Scripts/activate.bat
+++ learning_log/ll_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\iamgo\PycharmProjects\study-python\chap_18\learning_log\ll_env
+set VIRTUAL_ENV=C:\Users\iamgo\PycharmProjects\study-python\learning_log\ll_env
 
 if not defined PROMPT set PROMPT=$P$G
 
