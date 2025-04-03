5d837aa76fb331e12f81dd9c3089eb243ff07a5f
Ilya <iam.goryn@gmail.com>
2025-04-03 15:21:43 +0300

    refactoring the fill_walk() method

--- chap_15/15_3/random_walk.py
+++ chap_15/15_3/random_walk.py
@@ -8,13 +8,9 @@ class RandomWalk:
 
     def fill_walk(self):
         while len(self.x_values) < self.num_points:
-            x_direction = choice([1])
-            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
-            x_step = x_direction * x_distance
 
-            y_direction = choice([1])
-            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             if x_step == 0 and y_step == 0:
                 continue
@@ -24,3 +20,10 @@ class RandomWalk:
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+
+        return step
